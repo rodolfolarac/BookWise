6cffc8e6fa500454c75ef460c92afb898b3e13c0
Rodolfo Lara Cassaro <97470726+rodolfolarac@users.noreply.github.com>
2025-04-09 06:48:11 -0300

    feat:Método 1 de roteamento finalizados
    Adicionado método de roteamento e função para debugar código.

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-$controller = "index";
 
-if (isset($_SERVER['PATH_INFO'])) {
-    $controller = str_replace("/", "", $_SERVER['PATH_INFO']);
-}
 
-require "controllers/{$controller}.controller.php";
+require 'functions.php';
+require 'routes.php';
