054212b0d15c14ea079fd27f6e352e65a3b2f17e
Rodolfo Lara Cassaro <97470726+rodolfolarac@users.noreply.github.com>
2025-04-22 08:30:18 -0300

    Feat: Criação da página de login e cadastro

---
+++ views/login.view.php
@@ -0,0 +1,67 @@
+<div class="mt-6 grid grid-cols-2 gap-2">
+    <div class="border border-stone-700 rounded">
+        <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Login</h1>
+        <form class="p-4 space-y-4">
+            <div class="flex flex-col">
+                <label class="text-stone-400 mb-1">Email</label>
+                <input
+                    type="email"
+                    class="max-w-[100%] w-full border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 max-w-1/4"
+                    name="email" required>
+            </div>
+            <div class="flex flex-col">
+                <label class="text-stone-400 mb-1">Senha</label>
+                <input
+                    type="password"
+                    class="max-w-[100%] w-full border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 max-w-1/4"
+                    name="password" required>
+            </div>
+            <button type="submit"
+                class="border-stone-800 bg-stone-900 text-stone-400 px-4 py-1 rounded-md border border-2 hover:bg-stone-800 cursor-pointer">
+                Logar
+            </button>
+        </form>
+    </div>
+
+    <div class="border border-stone-700 rounded">
+        <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Registrar</h1>
+        <form class="p-4 space-y-4">
+            <div class="flex flex-col">
+                <label class="text-stone-400 mb-1">Nome</label>
+                <input
+                    type="text"
+                    class="max-w-[100%] w-full border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 max-w-1/4"
+                    name="name" required>
+            </div>
+            <div class="flex flex-col">
+                <label class="text-stone-400 mb-1">Email</label>
+                <input
+                    type="email"
+                    class="max-w-[100%] w-full border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 max-w-1/4"
+                    name="email" required>
+            </div>
+            <div class="flex flex-col">
+                <label class="text-stone-400 mb-1">Confirme seu email</label>
+                <input
+                    type="email"
+                    class="max-w-[100%] w-full border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 max-w-1/4"
+                    name="email_confirmacao" required>
+            </div>
+            <div class="flex flex-col">
+                <label class="text-stone-400 mb-1">Senha</label>
+                <input
+                    type="password"
+                    class="max-w-[100%] w-full border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 max-w-1/4"
+                    name="password" required>
+            </div>
+            <button type="submit"
+                class="border-stone-800 bg-stone-900 text-stone-400 px-4 py-1 rounded-md border border-2 hover:bg-stone-800 cursor-pointer">
+                Registrar
+            </button>
+            <button type="reset"
+                class="border-stone-800 bg-stone-900 text-stone-400 px-4 py-1 rounded-md border border-2 hover:bg-stone-800 cursor-pointer">
+                Cancelar
+            </button>
+        </form>
+    </div>
+</div>
