c31f20d7b7df8afe1ab2dea389f1f4457dc1a0c5
Rodolfo Lara Cassaro <97470726+rodolfolarac@users.noreply.github.com>
2025-04-29 05:28:27 -0300

    feat: Criação da validações do campo de cadastro de forma simples

--- controllers/registrar.controller.php
+++ controllers/registrar.controller.php
@@ -1,10 +1,42 @@
 <?php
-// GET --> Pegar
-// POST -> Enviar
-// PUT -> Atualizar blocos de informações
-// PATCH -> Atualizações pequenas de um campo só
-// DELETE ->Deletar
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $validacoes = [];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $email_confirmacao = $_POST['email_confirmacao'];
+    $senha = $_POST['senha'];
+
+    if (strlen($nome) == 0) {
+        $validacoes[] = 'O nome é obrigatório.';
+    }
+    if (strlen($email) == 0) {
+        $validacoes[] = 'O email é obrigatório.';
+    }
+    if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $validacoes[] = 'O email é inválidos.';
+    }
+    if ($email != $email_confirmacao) {
+        $validacoes[] = 'O email de confirmação está diferente.';
+    }
+    if (strlen($senha) == 0) {
+        $validacoes[] = 'A senha é obrigatório.';
+    }
+    if (strlen($senha) < 8 || strlen($senha) > 30) {
+        $validacoes[] = 'A senha precisa ter entre 8 e 30 caracteres.';
+    }
+    if (!str_contains($senha, '*')) {
+        $validacoes[] = 'A senha precisa ter um * no meio';
+    }
+    if (sizeof($validacoes) > 0) {
+        $validacoes = $_SESSION['validacoes'];
+        header('location: /login');
+        exit();
+    }
+
+
     $resultado = $database->query(
         query: "insert into usuarios (nome, email, senha) values ( :nome, :email, :senha)",
         params: [
--- views/login.view.php
+++ views/login.view.php
@@ -26,38 +26,51 @@
     <div class="border border-stone-700 rounded">
         <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Registrar</h1>
         <form method="POST" action="/registrar" class="p-4 space-y-4">
-            <?php if (strlen($mensagem) > 0): ?>
-                <div class="border-green-800 bg-green-900 text-green-400 px-4 py-1 rounded-md border border-2 hover:bg-stone-800">
+
+            <?php if (isset($mensagem) && strlen($mensagem)): ?>
+                <div class="border-green-800 bg-green-900 text-green-400 px-4 py-1 rounded-md border border-2 hover:bg-stone-800 text-sm font-bold">
                     <?= $mensagem ?>
                 </div>
             <?php endif; ?>
+
+            <?php if (isset($_SESSION['validacoes']) && sizeof($_SESSION['validacoes'])): ?>
+                <div class="border-red-800 bg-red-900 text-red-400 px-4 py-1 rounded-md border border-2 text-sm font-bold">
+                    <ul>
+                        <li>Deu ruim!</li>
+                        <?php foreach ($_SESSION['validacoes'] as $validacao): ?>
+                            <li><?= $validacao ?></li>
+                        <?php endforeach ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
             <div class="flex flex-col">
                 <label class="text-stone-400 mb-1">Nome</label>
                 <input
                     type="text"
                     class="max-w-[100%] w-full border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 max-w-1/4"
-                    name="nome" required>
+                    name="nome">
             </div>
             <div class="flex flex-col">
                 <label class="text-stone-400 mb-1">Email</label>
                 <input
                     type="email"
                     class="max-w-[100%] w-full border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 max-w-1/4"
-                    name="email" required>
+                    name="email">
             </div>
             <div class="flex flex-col">
                 <label class="text-stone-400 mb-1">Confirme seu email</label>
                 <input
                     type="email"
                     class="max-w-[100%] w-full border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 max-w-1/4"
-                    name="email_confirmacao" required>
+                    name="email_confirmacao">
             </div>
             <div class="flex flex-col">
                 <label class="text-stone-400 mb-1">Senha</label>
                 <input
                     type="password"
                     class="max-w-[100%] w-full border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1 max-w-1/4"
-                    name="senha" required>
+                    name="senha">
             </div>
             <button type="submit"
                 class="border-stone-800 bg-stone-900 text-stone-400 px-4 py-1 rounded-md border border-2 hover:bg-stone-800 cursor-pointer">
